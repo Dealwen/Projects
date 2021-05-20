07f8b949d41f989f439bee2db0ffaf768e29889f
Dealwen <79882125+Dealwen@users.noreply.github.com>
2021-05-20 21:49:15 +0300

    Consalting complete

---
+++ Альфа версия ТВ/js/main.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+    $(".owl-carousel").owlCarousel(
+      {
+        margin: 20,
+        autoplay: true,
+        autoplayTimeout: 4000,
+        items: 1,
+      }
+    );
+  });
